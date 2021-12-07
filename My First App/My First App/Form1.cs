955382fb4c9c7ae1c34c5a7a2c88d9903bc7d77d
Charles991 <95703531+Charles991@users.noreply.github.com>
2021-12-07 15:57:22 +0000

    Add files via upload

---
+++ My First App/My First App/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_First_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            MessageBox.Show("Hello " + name);
+        }
+    }
+}
